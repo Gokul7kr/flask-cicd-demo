d15c246c1313cec5478c0b0f0cde27d0d6d5b7ce
Gokul7kr <147075169+Gokul7kr@users.noreply.github.com>
2025-07-23 19:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def home():
     return "✅ Hello, Gokul! Your Flask app is working!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
